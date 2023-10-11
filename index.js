7f101528e5ef795f2df1a511c8a8436187413dce
SifatNiloy <sifatniloy18@gmail.com>
2023-10-11 12:15:17 +0600

    get post done

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app= express()
-const port = process.env.port || 5000 ;
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.port || 5000;
 
 //middleware
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 //  cofeeMaster
 //   St1ly9Obblz32krc
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nlpzidc.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri)
-app.get("/", (req, res)=>{
-    res.send("coffee maker is running")
-})
-app.listen(port, ()=>{
-    console.log(`coffee server is running on port, ${port}`)
-})
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const coffeeCollection = client.db("coffeeDB").collection("coffee");
+    app.get("/coffee", async (req, res) => {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.post("/coffee", async (req, res) => {
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+console.log(uri);
+app.get("/", (req, res) => {
+  res.send("coffee maker is running");
+});
+app.listen(port, () => {
+  console.log(`coffee server is running on port, ${port}`);
+});
